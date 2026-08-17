dae257c7bcacb4cce6945805bae5efd008b01618
ISHAAN KAPIL <146025286+IshaanKapil@users.noreply.github.com>
2026-08-18 00:50:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (70.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;      
-        int j = n - 1;      
-        int k = m + n - 1; 
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
+        // 1. Copy nums2 into the end of nums1
+        for (int i = 0; i < n; ++i) {
+            nums1[m + i] = nums2[i];
         }
 
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        int totalLen = m + n;
+        // 2. Initialize gap
+        int gap = (totalLen + 1) / 2;
+
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < totalLen) {
+                if (nums1[left] > nums1[right]) {
+                    swap(nums1[left], nums1[right]);
+                }
+                left++;
+                right++;
+            }
+
+            if (gap == 1) {
+                gap = 0;
+            } else {
+                gap = (gap + 1) / 2; // Ceil division
+            }
         }
     }
 };
